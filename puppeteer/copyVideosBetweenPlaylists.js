2463984ba281e9a3f046da19d74cdca4e234ec36
qaraluch <prezenciq@gmail.com>
2020-10-30 10:21:48 +0100

    Fix: check for deleted videos for copy videos between PLs task
    Changes to be committed:
    	modified:   puppeteer/copyVideosBetweenPlaylists.js

--- puppeteer/copyVideosBetweenPlaylists.js
+++ puppeteer/copyVideosBetweenPlaylists.js
@@ -4,7 +4,7 @@ const {
   getTagChildrenPositionByOuterText,
 } = require("./getTagChildrenPositionByOuterText");
 const { countTagChildrens } = require("../puppeteer/countTagChildrens");
-// const { takeScreenshot } = require("../puppeteer/takeScreenshot");
+const { takeScreenshot } = require("../puppeteer/takeScreenshot");
 
 async function copyVideosBetweenPlaylists(
   page,
@@ -16,6 +16,9 @@ async function copyVideosBetweenPlaylists(
   const videosCount = await countTagChildrens(page, selectorVideoListParent);
 
   for (let videoNo = resumeVideoNo; videoNo <= videosCount; videoNo++) {
+    // logging part
+    const videoInfoNo = ` - vid no. ${videoNo}:`;
+
     const selectorVideo = `ytd-playlist-video-list-renderer > #contents > ytd-playlist-video-renderer:nth-child(${videoNo})`;
 
     // get video's info for logging
@@ -34,16 +37,36 @@ async function copyVideosBetweenPlaylists(
     await button3Dots.click();
     await page.waitForSelector(selector3DotMenuPopup);
 
+    // FIX: check for deleted videos
+    const selector3DotMenuFirstItem =
+      "ytd-popup-container #items > ytd-menu-service-item-renderer:nth-child(1)";
+    const outerText1Item3DotMenu = await getTagOuterText(
+      page,
+      selector3DotMenuFirstItem
+    );
+    function checkIfRemoveFromItem(itmOuterText) {
+      return itmOuterText.trim().slice(0, 11) === "Remove from" ? true : false;
+    }
+    if (checkIfRemoveFromItem(outerText1Item3DotMenu)) {
+      // in case of deleted videos
+      // there is only one child no. 1 - remove from current PL
+      const selectorRemoveFromPL =
+        "ytd-popup-container #items > ytd-menu-service-item-renderer:nth-child(1) paper-item";
+      const buttonRemoveFromPL = await page.$(selectorRemoveFromPL);
+      await buttonRemoveFromPL.click();
+      console.log(
+        `${videoInfoNo} [x] seems this video may be deleted.                               [ Deleted! ]`
+      );
+      // skip rest of the loop; go to the next iteration
+      continue;
+    }
+
     // click save to playlist
     const selectorSaveToPlaylistButton =
       "ytd-popup-container #items > ytd-menu-service-item-renderer:nth-child(2) paper-item";
     //hardcoded that it is second child item, but layout shouldn't change too match
     const buttonSeaveToPlaylist = await page.$(selectorSaveToPlaylistButton);
     await buttonSeaveToPlaylist.click();
-    // selector: "ytd-popup-container paper-dialog #playlists.scrollable"
-    // WIP
-    // is buggy so waiting:
-    // await page.waitForTimeout(500);
     const selectorWaitForSaveToPlaylistPopup =
       "ytd-popup-container paper-dialog #playlists.ytd-add-to-playlist-renderer.scrollable";
     await page.waitForSelector(selectorWaitForSaveToPlaylistPopup);
@@ -92,11 +115,11 @@ async function copyVideosBetweenPlaylists(
     await buttonClosePopup.click();
 
     // logging part
-    const videoInfoNo = ` - vid no. ${videoNo}:`;
     const videoInfoTitle = `- ${videoChannelName} - ${truncate(
       videoTitle,
       30
     )}`;
+
     if (toSkip) {
       console.log(
         `${videoInfoNo} [x] already in: ${playlistTargetPosition}. '${playlistNameTarget}' [ Skipped! ] ${videoInfoTitle}`
